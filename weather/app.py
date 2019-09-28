30232ca33e2de2409f2486d235fef59f15a1f028
Matthias <matthiaswubs@gmail.com>
2019-09-28 17:01:11 +0200

    WEATHER - plot added

--- weather/app.py
+++ weather/app.py
@@ -6,6 +6,11 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from datetime import datetime as dt
 
+import pandas as pd
+df = pd.DataFrame({'x': range(24), 
+                   'y': [10,9,8,7.5,6.6,7,8.8,9,10,12,15,16,
+                         18,21,22,22,21.5,20,20,19.6,18.4,17,17,12]})
+
 app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
@@ -19,7 +24,22 @@ app.layout = html.Div(children=[
         max_date_allowed=dt(2020, 12, 31),
         initial_visible_month=dt.today(),
         date=str(dt.today())
-        )
+        ),
+
+    dcc.Graph(
+        id='plot',
+        style={'width': '800px'},
+        figure={
+            'data': [
+                {'x': df.x, 'y': df.y, 'type':'scatter', 'mode':'marker+lines'}
+            ],
+            'layout': {
+                'title': 'Temperature in Basel',
+                'yaxis': {'title': 'Temperature'},
+                'xaxis': {'title': 'Time'}
+            }
+        }
+    )
 ])
 
 @app.callback(
@@ -30,4 +50,4 @@ def text_output(t):
     return t
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
