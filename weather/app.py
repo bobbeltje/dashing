c7bafe5ce84a8391542db0c869a95b5a37ab3dab
bobbeltje <matthiaswubs@gmail.com>
2019-10-03 20:38:49 +0200

    working version

--- weather/app.py
+++ weather/app.py
@@ -33,14 +33,13 @@ def get_weather_data(l, date):
     year = int(date[:4])
     month = int(date[5:7])
     day = int(date[8:10])
-    print(f'{year}-{month}-{day}')
     
-    epoch = dt(year, month, day).timestamp()
+    epoch = int(dt(year, month, day).timestamp())
     base = "https://api.darksky.net/forecast/"
     key = "68c2f89966bbe863f2fd12d98370da08"
     lat = l.latitude                                                         
     lon = l.longitude
-    reques = f'{base}{key}/{lat},{lon},{int(epoch)}?units=si'
+    reques = f'{base}{key}/{lat},{lon},{epoch}?units=si'
     r = requests.get(reques)
     return r
         
@@ -50,7 +49,6 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
 
-    html.Button('Plot update', id='tst_button'),
     dcc.Input(id='location', type='text'),
     html.Div(id='typed_text'),
     html.Br(),
@@ -67,47 +65,34 @@ app.layout = html.Div(children=[
 
 @app.callback(
     Output('plot', 'figure'),
-    [Input('tst_button', 'n_clicks')]
-)
-def plot_weather(n_clicks):
-    global w
-    df = w.get_data()
-    df.iat[0, 1] = df.iat[0, 1] + 1
-    w.set_data(df)
-    p = {
-        'data': [
-            {'x': df.x, 'y': df.y, 'type':'scatter', 'mode':'marker+lines'}
-		],
-        'layout': {
-            'title': 'Temperature in Basel',
-            'yaxis': {'title': 'Temperature'},
-            'xaxis': {'title': 'Time'}
-		}
-	}
-        
-    return p
-
-@app.callback(
-    Output('typed_text', 'children'),
     [Input('get_data', 'n_clicks')],
     [State('location', 'value'), State('date_input', 'date')]
 )
 def text_output(n_clicks, value, dat):
-    if n_clicks is None :
-        return 'nothing'
-    print('\n\n\n\n\n\n\n\n\n\n')
-    global geolocator
+    if n_clicks is None:
+        return {'data':[]}
+    global w, geolocator
+    df = w.get_data()
     l = geolocator.geocode(value)
     if 'raw' in dir(l):
-        print(l.raw)
-        print('\n\n\n')
         r = get_weather_data(l, dat)
         weather = json.loads(r.text)
-        print(weather)
-        
-    value = '' if value is None else value
-    n_clicks = '' if n_clicks is None else str(n_clicks)
-    return value + n_clicks
+        weather = weather['hourly']['data']
+        x = [dt.fromtimestamp(i['time']) for i in weather]
+        y = [i['temperature'] for i in weather]
+        p = {
+            'data': [
+                {'x': x, 'y': y, 'type':'scatter', 'mode':'marker+lines'}
+            ],
+            'layout': {
+                'title': 'Temperature in ' + l.address,
+                'yaxis': {'title': 'Temperature'},
+                'xaxis': {'title': 'Time'}
+            }
+        }
+        return p
+    
+    return {'data':[]}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
